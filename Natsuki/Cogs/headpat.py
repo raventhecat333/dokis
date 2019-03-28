53c30956ea26c836d7fda13ac44e15b4d39cab46
Cheezy <33290752+Cheezy096@users.noreply.github.com>
2019-03-28 20:46:40 +0000

    Add files via upload

---
+++ Natsuki/Cogs/headpat.py
@@ -0,0 +1,21 @@
+import discord, random, asyncio
+from discord.ext import commands
+#Imports
+
+
+class General(commands.Cog):#Class thing no touchy!!!111
+
+    def __init__(self, bot):
+         self.b = bot #Please no touchy thx
+
+    @commands.command()
+    async def headpat(self,ctx): 
+        headpat_list = ["Hey! Don't pat me so hard!", "Geez, you're gonna mess up my hair!", "...okay, I guess that kinda felt nice...", "What do I look like, a puppy??", "T-thanks, I guess..."]
+        async with message.channel.typing():
+            await asyncio.sleep(0.4) 
+        await ctx.send(random.choice(headpat_list))
+
+
+
+def setup(bot):#No no child keep your hands off or this will break and not load
+    bot.add_cog(General(bot))
---
+++ Natsuki/Cogs/tickle.py
@@ -0,0 +1,20 @@
+import discord, random, asyncio
+from discord.ext import commands
+#Imports
+
+
+class General(commands.Cog):#Class thing no touchy!!!111
+
+    def __init__(self, bot):
+         self.b = bot #Please no touchy thx
+
+    @commands.command()
+    async def recipe(self,ctx): 
+        laughs = ["H-hey! Cut that out!! Ahahahaha!!", "Hehehehe!!", "Ehehehe!", "STOP IT! STOP! EHEHEHEHE!!!", "I'm gonna break your ribs for this! Hehehe!"]
+        async with ctx.message.channel.typing():
+            await asyncio.sleep(0.4) 
+        await ctx.send(random.choice(laughs))
+
+
+def setup(bot):#No no child keep your hands off or this will break and not load
+    bot.add_cog(General(bot))
