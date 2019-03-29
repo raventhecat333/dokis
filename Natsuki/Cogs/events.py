9ac2ecddce8a7867d5faaad46638dea6a788fc86
Cheezy <thecheese096@gmail.com>
2019-03-29 19:17:45 +0000

    Added most but not all of the tagging responses
    Please help me i think i'm going insane with this.

--- Natsuki/Cogs/events.py
+++ Natsuki/Cogs/events.py
@@ -55,6 +55,51 @@ class Event(client.Cog): #Silly man class leave alone thx
             await message.channel.send(random.choice(manga_list)) # Pick out something inside of manga_list and say it 
 
 
+
+            # -------------------------------------------------------Tagging-------------------------------------------------------
+        elif message.content.startswith(f"<@{self.b.user.id}>"):
+            message1 = message.content.split(" ")
+            if len(message1) == 1:
+                await message.channel.send("Yes")
+
+            elif "hi" in message.content or "hello" in message.content or "hey" in message.content:
+                hello_list = ["Hi, I guess...", "What, do I have to greet you back or something?", "Hey there, Dummy!"]
+                await message.channel.send(random.choice(hello_list))
+
+            elif "test" in message.content:
+                await message.channel.send("tagging me is working just fine.")
+
+            elif "i love you" in message.content or "ily" in message.content:
+                love_list = ["...I love you, too, okay??", "W-what?? Don't expect me to say that I love you back or anything, you d-dummy!", "*urk!* :flushed:", "Shut up! You don't mean that!"]
+                await message.channel.send(random.choice(love_list))
+
+            elif "good night" in message.content or "goodnight" in message.content:
+                goodnight_list = ["Goodnight, Dummy!", "Goodnight, then.", "You better get good rest or I'll punch you!", "Sleep well, baka!"]
+                await message.channel.send(random.choice(goodnight_list))
+
+            elif "good morning" in message.content or "goodmorning" in message.content:
+                goodmorning_list = ["Well, it's *A* morning, I guess...", "Good morning to everyone except my dad.", "Did you get a good night's sleep? Er, not that I really care!!"]
+                await message.channel.send(random.choice(goodmorning_list))
+
+            elif "good afternoon" in message.content or "goodafternoon" in message.content:
+                goodafternoon_list = ["Good afternoon, I guess.", "Afternoon.", "Yeah, so it's the afternoon. What's your point?"]
+                await message.channel.send(random.choice(goodafternoon_list))
+
+            elif "you are cute" in message.content or "you're cute" in message.content or "you are so cute" in message.content or "you're so cute" in message.content:
+                cute_list = ["***I'M NOT CUTE!!!***", "Hey! I'm not cute!", "Sh-shut up! I'm not cute!!", "Have you ever considered that maybe I want to be something other than cute?!"]
+                await message.channel.send(random.choice(cute_list))
+
+            elif "i apologise" in message.content or "sorry" in message.content:
+                apology_list = ["Hmph. I'll forgive you, but it's not like you deserve it!", "Fine. I guess I'll let it go...", "You better be sorry, you baka!"]
+                await message.channel.send(random.choice(apology_list))
+
+            elif "best girl" in message.content:
+                apology_list = ["Ha! It's obviously me!","Clearly i'm the best girl than all the other dokis"]
+
+            # -------------------------------------------------------Tagging-------------------------------------------------------
+
+
+            #Can someone else please work on this stupid tagging thing which is making this neat file look like a mess?
         
 def setup(bot):
     bot.add_cog(Event(bot))
