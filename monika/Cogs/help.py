279cb191dbe6f825a57a99a3843180d8e0073435
Shiro Rose <shirorose@lavenderlink.net>
2019-05-12 09:04:30 -0400

    Fixed Monika's help page to be hers
    Changed Monika's help page to not be natsukis

--- monika/Cogs/help.py
+++ monika/Cogs/help.py
@@ -11,20 +11,17 @@ class Help(client.Cog):#Class thing no touchy!!!111
 
     @client.command()
     async def help(self,ctx): 
-        embed = discord.Embed(title="Hey, it's me, Natsuki.", description="Freaking Cole decided to turn my .chr file into a file that lets me be on this Discord server. It's not like I'm here because I want to be or anything, you dummies! A-anyway, here are the things I can do:", color=conf.norm)
-        embed.add_field(name="n_ask", value="You can ask me any yes-or-no question with this. But don't get mad if I don't know the answer or if I give the wrong answer!", inline=True)
-        embed.add_field(name="n_feed", value="Y-you don't have to or anything, but I guess you could feed me some food i-if you really wanted to... *(Format like this: 'n_feed :food_emoji:')*", inline=True)
-        embed.add_field(name="n_headpat", value="You can use this to pat me on the head, but why any normal person would want to, I have no clue...", inline=True)
-        embed.add_field(name="n_hug", value="Ugh. Hugs are gross, but if you want me to hug you or anyone on the server, I guess I can do that for you... Just leave it blank if you want me to hug you or @mention someone to have me hug them. *(Format like this: n_hug @mention)*", inline=True)
-        embed.add_field(name="n_invite", value="You can use this to show the link to invite me to other servers. But don't think that I'll enjoy it, you dummy!", inline=True)
-        embed.add_field(name="n_motivate", value="I-it's not like I want to, but if you need motivation or encouragement, I'll try to help you out, I guess.", inline=True)
-        embed.add_field(name="n_quote", value="You can use this to make me say one of my quotes from the game.", inline=True)
-        embed.add_field(name="n_recipe", value="Do you like baking, like me? Well, I've got a few recipes in my cookbook; feel free to check them out!", inline=True)
-        embed.add_field(name="n_tickle", value="You can use this to... TICKLE ME?? Oh, don't you dare use that unless you want a trip to the hospital!", inline=True)
-        embed.add_field(name="@Natsuki", value="Use this to get my attention, if you want. You can even try to use certain words or phrases to get certain responses. But that doesn't mean I'll understand everything you say!", inline=True)
-        embed.add_field(name="I guess that's it...", value="Cole will make me do more stuff soon, but I'm not looking forward to it! If you want, you can go visit his Support Server and give him a piece of my mind! Anyway, see you later, ~~bakas~~ everyone!", inline=True)
+        embed = discord.Embed(title="Greetings! I'm Monika!", description="Cole ~~finally~~ turned my .chr file into a .py file so I can join Discord! I'm not fully self-aware like I was in DDLC, but this will have to suffice!", color=0x12ba01)
+        embed.add_field(name="m_ask", value="Use this to ask me a yes-or-no question. I'm admittedly not the smartest person ~~in the game~~ on Earth, but I'll try my hardest to answer correctly!", inline=True)
+        embed.add_field(name="m_delete", value="Do you need something 'deleted' from your server? I'll be happy to help!", inline=True)
+        embed.add_field(name="m_feed", value="Would you like to feed me something? You can with this command!", inline=True)
+        embed.add_field(name="m_headpat", value="Ahaha! I don't understand the appeal of patting girls on the head, but I suppose I can figure it out...", inline=True)
+        embed.add_field(name="m_hug", value="I'm always open to giving a hug to anyone who wants one! Or, let's be honest, even if they *don't* want one!", inline=True)
+        embed.add_field(name="m_tickle", value="O-Oh! Well, I guess there are worse things to do to me than tickling!", inline=True)
+        embed.add_field(name="I do believe that's it!", value="I'm still in very early stages of development, but if Cole was able to create the other girls quickly, I'm sure I'll be finished before you know it!")
         embed.set_footer(text="Support Server: https://discord.gg/QnzsG38")
-        await ctx.send(embed=embed)
+        await channel.send(embed=embed)
+        return
 
 
 def setup(bot):#No no child keep your hands off or this will break and not load
