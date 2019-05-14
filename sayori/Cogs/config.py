a5ef069da581d2e755716c12f09a1ec4708c271b
Cheezy <33290752+Cheezy096@users.noreply.github.com>
2019-05-14 16:35:32 +0100

    Added Testing Mode (Yuri)

--- sayori/Cogs/config.py
+++ sayori/Cogs/config.py
@@ -9,6 +9,7 @@ class conf():
     type_speed = 2
     playing_msg = [f"Type '{prefix}help' for help!","Doki Doki Literature Club","with the crayons!","Katawa Shoujo","with Mr. Cow!"]
     admins = [480580173431832577, 279732337971953664, 332271541859647498, 218085039768993803, 310496481435975693, 270057011251642368]
+    test_mode = False
 
     ''' Just wanted to clear out that these hex codes bellow are for embed colours so i don't have to keep changing them in every single fucking file '''
     err = 0xff42e2 # The Error Embed Colour
@@ -22,8 +23,15 @@ class conf():
 
 
     ''' Doki Bot's ID'S '''
-    natsuki_id = 531555963908653076
-    monika_id = 531556928732528670 
-    sayori_id = 531554745337249792
-    yuri_id = 531556397746356224
+    if test_mode is True:
+        natsuki_id = 531555963908653076
+        monika_id = 531556928732528670 
+        sayori_id = 531554745337249792
+        yuri_id = 531556397746356224
+
+    elif test_mode is False:
+        natsuki_id = 433834936450023424
+        monika_id = 436351740787294208 
+        sayori_id = 425696108455657472
+        yuri_id = 436350586670153730
 
