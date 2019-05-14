837114b1849cf1f82d7e42a4abb928d68eb70327
Cheezy <33290752+Cheezy096@users.noreply.github.com>
2019-05-14 16:26:25 +0100

    Added Testing Mode (Monika)

--- monika/Cogs/config.py
+++ monika/Cogs/config.py
@@ -7,8 +7,9 @@ class conf():
     sharding = False
     cogd = "Cogs"
     type_speed = 2
-    playing_msg = ["Type 'm_help' for help!","Doki Doki Literature Club","the piano!","Super Smash Bros Ultimate.", "If My Heart Had Wings"]
+    playing_msg = ["Type 'm_help' for help!","Doki Doki Literature Club","Don't Starve","Cooking Mama"]
     admins = [480580173431832577, 279732337971953664, 332271541859647498, 218085039768993803, 310496481435975693, 270057011251642368]
+    test_mode = False
 
     ''' Just wanted to clear out that these hex codes bellow are for embed colours so i don't have to keep changing them in every single fucking file '''
     err = 0xC91313 # The Error Embed Colour
@@ -20,12 +21,19 @@ class conf():
     everyone_tag = "Hey! Do you **WANT** everyone to freak out in the chat?! Because I won't let you do that!"
     econfused = "Uh... What?"
 
-
+    
     ''' Doki Bot's ID'S '''
-    natsuki_id = 531555963908653076
-    monika_id = 531556928732528670 
-    sayori_id = 531554745337249792
-    yuri_id = 531556397746356224
+    if test_mode is True:
+        natsuki_id = 531555963908653076
+        monika_id = 531556928732528670 
+        sayori_id = 531554745337249792
+        yuri_id = 531556397746356224
+
+    elif test_mode is False:
+        natsuki_id = 433834936450023424
+        monika_id = 436351740787294208 
+        sayori_id = 425696108455657472
+        yuri_id = 436350586670153730
 
 
 
