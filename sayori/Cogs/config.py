93c14dcb90b60947686a9ff01e515791b3c0dc59
Cheezy <33290752+Cheezy096@users.noreply.github.com>
2019-05-19 20:11:17 +0100

    Preparing Launch (Sayori p2)

--- sayori/Cogs/config.py
+++ sayori/Cogs/config.py
@@ -1,15 +1,16 @@
 class conf():
 
-	token = "token"
-    prefix = "s_"
+    token = "token"
+    prefix1 = "s_"
+    prefix2 = "S_"
     name = "Sayori"
-    version = "1.0a"
-    sharding = False
+    version = "1.0L Biscuit"
+    sharding = True
     cogd = "Cogs"
     type_speed = 2
-    playing_msg = [f"Type '{prefix}help' for help!","Doki Doki Literature Club","with the crayons!","Katawa Shoujo","with Mr. Cow!"]
+    playing_msg = [f"Type 's_help' for help!","Doki Doki Literature Club","with the crayons!","Katawa Shoujo","with Mr. Cow!"]
     admins = [480580173431832577, 279732337971953664, 332271541859647498, 218085039768993803, 310496481435975693, 270057011251642368]
-    test_mode = True
+    test_mode = False
 
     ''' Just wanted to clear out that these hex codes bellow are for embed colours so i don't have to keep changing them in every single fucking file '''
     err = 0xff42e2 # The Error Embed Colour
