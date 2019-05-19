85435203050511e1fccc839f8f454bdfc7a77801
Cheezy <33290752+Cheezy096@users.noreply.github.com>
2019-05-19 20:10:05 +0100

    Preparing Launch (Natsuki p1)

--- natsuki/Cogs/config.py
+++ natsuki/Cogs/config.py
@@ -1,15 +1,16 @@
 class conf():
 
-	token = "token"
-    prefix = "n_"
+    token = "token"
+    prefix1 = "n_"
+    prefix2 = "N_"
     name = "Natsuki"
-    version = "1.0a"
-    sharding = False
+    version = "1.0L Biscuit"
+    sharding = True
     cogd = "Cogs"
     type_speed = 2
     playing_msg = ["Type 'n_help' for help!","Doki Doki Literature Club","Don't Starve","Cooking Mama"]
     admins = [480580173431832577, 279732337971953664, 332271541859647498, 218085039768993803, 310496481435975693, 270057011251642368]
-    test_mode = True
+    test_mode = False
 
     ''' Just wanted to clear out that these hex codes bellow are for embed colours so i don't have to keep changing them in every single fucking file '''
     err = 0xC91313 # The Error Embed Colour
