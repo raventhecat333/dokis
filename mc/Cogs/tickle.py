461e082168e70b638f7e36da269d053bf0fd81cd
Albert Tang <tangalbert919@yahoo.com>
2019-07-10 17:13:22 -0700

    Please don't tickle me. - MC

--- mc/Cogs/tickle.py
+++ mc/Cogs/tickle.py
@@ -4,18 +4,20 @@ from Cogs.config import conf
 #Imports
 
 
-class Tickle(client.Cog):#Class thing no touchy!!!111
+class Tickle(client.Cog):
 
     def __init__(self, bot):
          self.b = bot #Please no touchy thx
 
     @client.command()
+    @client.guild_only()
     async def tickle(self,ctx): 
-        laughs = ["Hehehehehe!~", "Ahahahaha!!", "*giggles*", "**PFFFT AHAHAHAHAHAHHAHAHAHHAHA!!!!!**", "Ehehe!~", "WAHAHAHAHAHA!!!~"]        
+        #laughs = ["Hehehehehe!~", "Ahahahaha!!", "*giggles*", "**PFFFT AHAHAHAHAHAHHAHAHAHHAHA!!!!!**", "Ehehe!~", "WAHAHAHAHAHA!!!~"]        
         async with ctx.message.channel.typing():
             await asyncio.sleep(conf.type_speed)  
-        await ctx.send(random.choice(laughs))
+        #await ctx.send(random.choice(laughs))
+        await ctx.send("I'm not ticklish, please stop.")
 
 
-def setup(bot):#No no child keep your hands off or this will break and not load
+def setup(bot):
     bot.add_cog(Tickle(bot))
