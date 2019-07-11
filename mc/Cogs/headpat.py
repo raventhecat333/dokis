3b7a9fd83d18c5eb3e14ea66c24506c55c420535
Albert Tang <tangalbert919@yahoo.com>
2019-07-10 19:32:53 -0700

    *pats MC's head* - iDroid

--- mc/Cogs/headpat.py
+++ mc/Cogs/headpat.py
@@ -4,19 +4,17 @@ from Cogs.config import conf
 #Imports
 
 
-class Headpat(client.Cog):#Class thing no touchy!!!111
+class Headpat(client.Cog):
 
     def __init__(self, bot):
          self.b = bot #Please no touchy thx
 
     @client.command()
-    async def headpat(self,ctx): 
-        headpat_list = ["Hehehe!~", "Just don't mess up my bow!", "S-stop being so silly! :blush:", "Well, my hair's already pretty messy, so I don't see an issue!", "Hehehe! Thank you!"]        
+    async def headpat(self,ctx):   
         async with ctx.message.channel.typing():
-            await asyncio.sleep(conf.type_speed)  
-        await ctx.send(random.choice(headpat_list))
+            await asyncio.sleep(conf.type_speed)
+        await ctx.send("Stop it!")
 
 
-
-def setup(bot):#No no child keep your hands off or this will break and not load
+def setup(bot):
     bot.add_cog(Headpat(bot))
