71a6333b5d8093fe5ff8709cf26511262b5130b5
Albert Tang <tangalbert919@yahoo.com>
2019-07-11 00:30:26 -0700

    Change the last two responses. - iDroid

--- mc/Cogs/help.py
+++ mc/Cogs/help.py
@@ -21,8 +21,8 @@ class Help(client.Cog):
         embed.add_field(name="mc_tickle", value="I'm not tickleish, don't bother.", inline=True)
         embed.add_field(name="mc_changelog", value="Check out what's been changed!", inline=True)
         embed.add_field(name="mc_confess", value="Don't use this at all!", inline=True)
-        embed.add_field(name="@MC", value="Use this to either get my attention or to use special 'trigger words/phrases' to get certain responses out of me! Type 'mc_commands' for a full list!", inline=True)
-        embed.add_field(name="And that's about it!", value="I'm sure Cole will add more stuff for me to do soon, but for now, I hope you enjoy my presence on the server! If you have any questions, comments, or bugs, let Cole know! Oh, and please don't be a meanie :unamused:. That's all for now. Bye!~")
+        embed.add_field(name="@MC", value="So use this I think to get my attention. You can also type 'mc_commands' for a full list of what I can do (not much) or mc_invite if for some reason you want me on your server.", inline=True)
+        embed.add_field(name="I think that's all about me, most likely Cole will make me do more things (sadly) if I’m buggy blame Monika and let Cole know if you have any questions I guess, I’m going back to sleep now~")
         embed.set_footer(text="Support Server: https://discord.gg/QnzsG38")
         await ctx.send(embed=embed)
 
