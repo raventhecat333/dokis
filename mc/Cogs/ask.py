bab5a99c7dea39d30399e83af674d4db132f6494
Albert Tang <tangalbert919@yahoo.com>
2019-07-10 21:15:45 -0700

    Ask me anything, nerd. - MC

--- mc/Cogs/ask.py
+++ mc/Cogs/ask.py
@@ -3,22 +3,22 @@ from discord.ext import commands as client
 #Imports
 
 
-class Ask(client.Cog):#Class thing no touchy!!!111
+class Ask(client.Cog):
 
     def __init__(self, bot):
-         self.b = bot #Please no touchy thx
+         self.b = bot
 
     @client.command()
-    async def ask(self,ctx, arg1=None): # we make arg1 so we can have the command as this "n_ask my dad is in jail lmao" and it will obviously respond, if your missing the "answer arg" which comes after the command then the command will obviously not run
+    @client.guild_only()
+    async def ask(self, ctx, arg1=None):
         if arg1 is None:
-            await ctx.send("I can't answer the question if you don't ask one, silly!")
+            await ctx.send("I can't answer a non-existent question.")
         else:
-            answer_list = ["Yes!", "No.", "Maybe.", "Possibly?", "Of course, silly!", "I'd say ask Monika, but she's busy being ~~a meanie~~ an amazing club president!", "I'd say ask Yuri, but she's a little shy at the moment.", "I'd say ask Natsuki, but she's busy baking some cookies for ~~me to steal~~ the club!", "You've got a better chance of having a happy ending in DDLC! Ehehe...~", "Maybe we should ask The Magic Conch, instead.", "As sure as I'm depressed!", "Not really.", "My Vice President Powers tell me yes!", "My Vice President Powers tell me no!", "My Vice President Powers tell me maybe!", "J-Just a little bit!"]            
+            answer_list = ["I don't know why you're asking me, go ask Sayori.", "Yes, I guess.", "No, I think; I don't care either way.", "Maybe? Monika would know.", "Yeh.", "No, just no."]            
             async with ctx.message.channel.typing():
-                await asyncio.sleep(0.4) 
+                await asyncio.sleep(conf.type_speed)
             await ctx.send(random.choice(answer_list))
 
 
-
-def setup(bot):#No no child keep your hands off or this will break and not load
+def setup(bot):
     bot.add_cog(Ask(bot))
