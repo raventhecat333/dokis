12872c32de15ebf580e3aa5377c56054bb33dec1
Albert Tang <tangalbert919@yahoo.com>
2019-07-11 00:02:34 -0700

    Ask me for help if you need to... - MC

--- mc/Cogs/help.py
+++ mc/Cogs/help.py
@@ -4,32 +4,29 @@ from Cogs.config import conf
 #Imports
 
 
-class Help(client.Cog):#Class thing no touchy!!!111
+class Help(client.Cog):
 
     def __init__(self, bot):
-         self.b = bot #Please no touchy thx
+         self.b = bot 
 
     @client.command()
     async def help(self,ctx): 
-        embed = discord.Embed(title="Hi! I'm Sayori!", description="Cole was kind enough to turn my .chr file into a file that can let me interact with you guys to a certain extent! My commands are as follows:", color=conf.norm)
-        embed.add_field(name="s_ask", value="Use this to ask me a yes-or-no question and receive an answer! Will I always be correct? Probably not, but my answers could yield some silly results!~", inline=True)
-        embed.add_field(name="s_feed", value="Use this to feed me any of the foods available in the 'food' section of the Discord Emojis! Don't worry, I have a big stomach, so you can feed me as much as you want! *(Format like this: s_feed :food_emoji:)*", inline=True)
-        embed.add_field(name="s_headpat", value="Use this to pat me on the head! :grin:", inline=True)
-        embed.add_field(name="s_hug", value="Use this to have me hug someone! Leave it blank to have me hug you, or mention someone to have me hug them! *(Format like this: s_hug @mention)*", inline=True)
-        embed.add_field(name="s_invite", value="Use this to put my Discord invite link in the chat so anyone can invite me to their own server!", inline=True)
-        embed.add_field(name="s_joke", value="Use this to have me tell a random joke!", inline=True)
-        embed.add_field(name="s_lifeline", value="I-Is someone on the server talking about ending their life...? Use this to pull up the National Suicide Prevention Lifeline. It doesn't really guarantee that they'll call it, but at least it's something you can do to try and help!", inline=True)
-        embed.add_field(name="s_poems", value="Use this to read one of the poems from Doki Doki Literature Club!", inline=True)
-        embed.add_field(name="s_quote", value="Use this to have me say one of my quotes from the game!", inline=True)
-        embed.add_field(name="s_swear", value="Use this to have me swear! Why you would want me to, I'm not sure, but the option is there!", inline=True)
-        embed.add_field(name="s_tickle", value="Use this to make me laugh!", inline=True)
-        embed.add_field(name="s_changelog", value="Check out what's been changed!", inline=True)
-        embed.add_field(name="@Sayori", value="Use this to either get my attention or to use special 'trigger words/phrases' to get certain responses out of me! Type 's_commands' for a full list!", inline=True)
+        embed = discord.Embed(title="I'm MC, I guess.", description="So somehow my non-existent .chr file was turned into a discord bot file, or whatever the hell it's called, anyways I'm here because Sayori dragged me here. I don't have any talents or anything so I don't know why I'm here.", color=conf.norm)
+        embed.add_field(name="mc_ask", value="You can ask me anything, I really don't care.", inline=True)
+        embed.add_field(name="mc_feed", value="I'll eat really anything, but I'm fine with my own food.", inline=True)
+        embed.add_field(name="mc_headpat", value="Useless command.", inline=True)
+        embed.add_field(name="mc_hug", value="You can hug me, I don't know why but you can.", inline=True)
+        embed.add_field(name="mc_invite", value="I don't know why but you can invite me to your server, if you want to.", inline=True)
+        embed.add_field(name="mc_quote", value="You can make me say a quote from the game, I don't know why you would want to, but you can.", inline=True)
+        embed.add_field(name="mc_tickle", value="I'm not tickleish, don't bother.", inline=True)
+        embed.add_field(name="mc_changelog", value="Check out what's been changed!", inline=True)
+        embed.add_field(name="mc_confess", value="Don't use this at all!", inline=True)
+        embed.add_field(name="@MC", value="Use this to either get my attention or to use special 'trigger words/phrases' to get certain responses out of me! Type 'mc_commands' for a full list!", inline=True)
         embed.add_field(name="And that's about it!", value="I'm sure Cole will add more stuff for me to do soon, but for now, I hope you enjoy my presence on the server! If you have any questions, comments, or bugs, let Cole know! Oh, and please don't be a meanie :unamused:. That's all for now. Bye!~")
         embed.set_footer(text="Support Server: https://discord.gg/QnzsG38")
         await ctx.send(embed=embed)
 
 
-def setup(bot):#No no child keep your hands off or this will break and not load
+def setup(bot): 
     bot.remove_command("help")
     bot.add_cog(Help(bot))
