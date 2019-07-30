8bc691ae6791866a890fe269b7007a9c84386a03
Cheezy <thecheese096@gmail.com>
2019-07-30 13:57:24 +0100

    Changed "CS" to full name

--- monika/Cogs/config.py
+++ monika/Cogs/config.py
@@ -14,10 +14,10 @@ class conf():
 
     if test_mode is False:
         sharding = True
-        version = "2.2L CS"
+        version = "2.2L Club Sandwich"
     else:
         sharding = False
-        version = "2.2B CS" # Testing mode should be beta.
+        version = "2.2B Club Sandwich" # Testing mode should be beta.
     #L|Launch    B|Beta
 
     ''' Just wanted to clear out that these hex codes bellow are for embed colours so i don't have to keep changing them in every single fucking file '''
