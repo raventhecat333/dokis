fb2c1a6e8a1fe3112bc1449ba912be2617b885d5
Albert Tang <tangalbert919@yahoo.com>
2019-08-02 09:20:52 -0700

    Fix hug response bug for Sayori and Natsuki
    Resolves #54 and #56

--- sayori/Cogs/hug.py
+++ sayori/Cogs/hug.py
@@ -4,10 +4,10 @@ from Cogs.config import conf
 #Imports
 
 
-class Hug(client.Cog):#Class thing no touchy!!!111
+class Hug(client.Cog):
 
     def __init__(self, bot):
-         self.b = bot #Please no touchy thx
+         self.b = bot
 
     @client.command()
     async def hug(self,ctx, *, message=None): 
@@ -16,24 +16,24 @@ class Hug(client.Cog):#Class thing no touchy!!!111
             user = ctx.author
             hug_list = [f"One hug, coming right up! *hugs <@{user.id}>*", f"I'll try not to squeeze too hard! *hugs <@{user.id}>*", f"Time for the super-mega-cinnamon-bun hug! *hugs <@{user.id}>*", f"How could I say no to a hug? *hugs <@{user.id}>*", f"Yay, hugs! *hugs <@{user.id}>*"]
             async with ctx.message.channel.typing():
-                await asyncio.sleep(conf.type_speed)  
+                await asyncio.sleep(conf.type_speed)
             await ctx.send(random.choice(hug_list))
 
         elif message == '@everyone' or message == '@here':
             await ctx.send(conf.everyone_tag)
             
-        if message == f'<@{self.b.user.id}>': # Oh noes it's me!
+        elif message == f'<@{self.b.user.id}>': # Oh noes it's me!
             hug_list = [f"...fine. *hugs myself*", "Well, if you say so... *hugs myself*", "*hugs myself* Huh. Now I see why you guys like my hugs so much."]
             async with ctx.message.channel.typing():
-                await asyncio.sleep(conf.type_speed)  
+                await asyncio.sleep(conf.type_speed)
             await ctx.send(random.choice(hug_list))
 
         else: # Argument, okay let's spit whatever the user just said
-            hug_list = [f"One hug, coming right up! *hugs {message}*", f"I'll try not to squeeze too hard! *hugs {message}*", f"Time for the super-mega-cinnamon-bun hug! *hugs {message}*", f"How could I say no to a hug? *hugs {message}*", f"Yay, hugs! *hugs {message}*"] 
+            hug_list = [f"One hug, coming right up! *hugs {message}*", f"I'll try not to squeeze too hard! *hugs {message}*", f"Time for the super-mega-cinnamon-bun hug! *hugs {message}*", f"How could I say no to a hug? *hugs {message}*", f"Yay, hugs! *hugs {message}*"]
             async with ctx.message.channel.typing():
-                await asyncio.sleep(conf.type_speed)  
+                await asyncio.sleep(conf.type_speed)
             await ctx.send(random.choice(hug_list))
 
 
-def setup(bot):#No no child keep your hands off or this will break and not load
+def setup(bot):
     bot.add_cog(Hug(bot))
