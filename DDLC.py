2e34bac81abddf156c2448b14906a1cbaf833cdb
Yuri's Husband <53578770+YurisHusband@users.noreply.github.com>
2020-11-04 18:19:28 +0200

    Add files via upload

--- DDLC.py
+++ DDLC.py
@@ -1,24 +1,11 @@
-import asyncio, chrs, json, os, sqlite3, subprocess, sys, threading, traceback
+import asyncio, discord, json, os
 from bot import Character
 
-def run_chr(chr):
-    try:
-        asyncio.set_event_loop(asyncio.new_event_loop())
-        chr = chrs.loadChr(chr[:-3])
-        if config["test bot"]["active"]:
-            chr["character"].id = config["test bot"]["id"]
-            chr["character"].token = config["test bot"]["token"]
-        character = Character(chr=chr)
-        character.run(chr["character"].token)
-    except Exception as error:
-        print(f"Could not run {chr['name']}.py!")
-        print(traceback.format_exc())
-        print(error)
+loop = asyncio.get_event_loop()
 
-characters = chrs.getCharacters()
+for chrfile in [file for file in os.listdir('./characters') if file.endswith('.chr')]:
+    
+    chrjson = json.loads(open(f"characters/{chrfile}", "r").read())
+    loop.create_task(Character(chrjson).start(chrjson["about"]["token"]))
 
-config = json.loads(open("config.json", "r").read())
-
-for chr in [file for file in os.listdir('./characters') if file.endswith('.py')]:
-    threading.Thread(target=run_chr, args=(chr,)).start()
-    print(f'{chr} has successfully been recognized!')
+loop.run_forever()
