c2227b431b0683ed9c880f5cab71256e308f9111
Yuri's Husband <53578770+YurisHusband@users.noreply.github.com>
2020-11-06 17:22:43 +0200

    re-upload

---
+++ DDLC.py
@@ -0,0 +1,12 @@
+import asyncio, discord, json, os
+from bot import Character
+from loop import loop
+
+for chrfile in [file for file in os.listdir('./characters') if file.endswith('.chr')]:
+
+    chrjson = json.loads(open(f"characters/{chrfile}", "r").read())
+    tokensjson = json.loads(open(f"tokens.json", "r").read())
+    loop.characters.append(Character(chrjson))
+    loop.create_task(loop.characters[-1].start(tokensjson[chrfile[:-4]]), name=chrjson["about"]["name"])
+
+loop.run_forever()
